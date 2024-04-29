9598d041b6ad60bd498ef2c6f5027092f23c1e5d
gori <justitiarulfurtunii007@gmail.com>
2024-04-29 14:50:00 +0200

    remove

--- projects/ecu_scheme/assemble/lumped_mass_matrix_provider.cc
+++
@@ -1,8 +0,0 @@
-// Project header file include
-// C system headers
-// C++ standard library headers
-// Other libraries headers
-
-#include "lumped_mass_matrix_provider.h"
-
-namespace ecu_scheme::assemble {}  // namespace ecu_scheme
